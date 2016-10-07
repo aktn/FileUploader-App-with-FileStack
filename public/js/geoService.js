eba9a852f2d186c48912253887361cb93928097d
Aung Khant <theaungkhantl@gmail.com>
2016-10-07 20:55:06 +0630

    Adding clickability and dragging pin on the map

--- public/js/geoService.js
+++ public/js/geoService.js
@@ -3,6 +3,9 @@ angular.module('geoService', [])
 
         var googleMapService = {};
 
+        googleMapService.clickLat  = 0;
+        googleMapService.clickLong = 0;
+
         // Array of locations
         var locations = [];
 
@@ -101,6 +104,28 @@ var initialize = function(latitude, longitude) {
         icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
     });
     lastMarker = marker;
+
+    // Function for moving to a selected location
+    map.panTo(new google.maps.LatLng(latitude, longitude));
+
+    // Clicking on the Map moves the bouncing red marker
+    google.maps.event.addListener(map, 'click', function(e){
+        var marker = new google.maps.Marker({
+            position: e.latLng,
+            animation: google.maps.Animation.BOUNCE,
+            map: map,
+            icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+        });
+
+        // When a new spot is selected, delete the old red bouncing marker
+        if(lastMarker){
+            lastMarker.setMap(null);
+        }
+
+        // Create a new red bouncing marker and move to it
+        lastMarker = marker;
+        map.panTo(marker.position);
+    });
 
 };
 
